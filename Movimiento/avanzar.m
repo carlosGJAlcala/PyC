8524c10451359fa33d04ad4198bad408a6b1be5f
carlos.garridoj@edu.uah.es <carlos.garridoj@edu.uah.es>
2024-02-15 13:25:07 +0100

    se ha añadido un array para guardar un historico de la velocidad angular y la velocidad lineal

--- Movimiento/avanzar.m
+++ Movimiento/avanzar.m
@@ -1,19 +1,26 @@
 distancia=4.0;
 r=robotics.Rate(10);
-msg_vel.Linear.X=0.1;
-msg_vel.Linear.Y=0.1;
+msg_vel.Linear.X=0.7;
+msg_vel.Linear.Y=0.0;
 msg_vel.Linear.Z=0;
 msg_vel.Angular.X=0;
 msg_vel.Angular.Y=0;
 msg_vel.Angular.Z=0;
 
+ruta_seguida=[];
+
+
 %Leemos la primera posicion
 initpos = sub_odom.LatestMessage.Pose.Pose.Position;
 disp("Inicializamos leyendo la primera posicion: ");
-
+i=0;
 while(1)
+    i=i+1
     pos=sub_odom.LatestMessage.Pose.Pose.Position;
     dist=sqrt((initpos.X-pos.X)^2+(initpos.Y-pos.Y)^2);
+    dist
+     ruta_seguida(i)=dist;
+
     if(dist>distancia)
         msg_vel.Linear.X=0;
         send(pub_vel,msg_vel);
--- conectar/conectar.m
+++ conectar/conectar.m
@@ -1,4 +1,4 @@
-rosinit('http://172.29.29.68:11311','NodeHost','172.29.29.67')
+rosinit('http://172.22.65.206:11311','NodeHost','172.22.66.45')
 %Universidad
  % rosinit('http://172.22.55.198:11311','NodeHost','172.22.66.45')
   %rosinit('http://172.29.30.179:11311','NodeHost','172.29.29.110')
